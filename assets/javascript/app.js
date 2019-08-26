26169728400a5d0be71917ad234fe3c5ed074351
jastring <tripp.stringfield@gmail.com>
2019-08-26 11:06:57 -0400

    added some debugging

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,36 +6,47 @@ var config = {
     storageBucket: "",
     messagingSenderId: "570706840985",
     appId: "1:570706840985:web:66af9a4c6d8e3d14"
-  };
-  
-  firebase.initializeApp(config);
-  var database = firebase.database();
+};
+
+firebase.initializeApp(config);
+var database = firebase.database();
 
 
-  $("#add-train").on("click", function(event) {
+$("#add-train").on("click", function (event) {
     event.preventDefault();
 
-    // Grabbed values from text boxes
     name = $("#name-input").val().trim();
     destination = $("#destination-input").val().trim();
+    first = moment($("#first-input").val().trim(), "HH:mm").subtract(1, "years").format("X");
     frequency = $("#frequency-input").val().trim();
 
-    // Code for handling the push
-    database.ref().push({
-      name: name,
-      destination: destination,
-      frequency: frequency
-    });
+    if (name === "" || destination === "" || first === "" || frequency === "") {
+        alert("Invalid Form. Try again")
+    } else if (isNaN(first) || isNaN(frequency)) {
+        alert("Invalid Times. Try again")
+    } else {
+
+        database.ref().push({
+            name: name,
+            destination: destination,
+            first: first,
+            frequency: frequency
+        });
+    }
+    $("#name-input").val("");
+    $("#destination-input").val("");
+    $("#first-input").val("");
+    $("#frequency-input").val("");
 });
 
-database.ref().on("child_added", function(snapshot) {
+database.ref().on("child_added", function (snapshot) {
     var sv = snapshot.val();
-    var months = moment().diff(moment(sv.start),"months");
-    $("#table-body").append("<tr>" + "<td>" + sv.name + "</td><td>" + sv.destination + "</td><td>" + sv.frequency + "</td> </tr>");
+    var timeDiff = moment().diff(moment.unix(sv.first), "minutes");
+    var timeRemainder = timeDiff % sv.frequency;
+    var minTill = sv.frequency - timeRemainder;
+    var nextTrain = moment().add(minTill, "m").format("hh:mm A");
+    $("#table-body").append("<tr>" + "<td>" + sv.name + "</td><td>" + sv.destination + "</td><td>" + sv.frequency + "</td><td>" + nextTrain + "</td><td>" + minTill + "</td> </tr>");
 
-
-    // Handle the errors
-  }, function(errorObject) {
+}, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
-  });
-  
+});
